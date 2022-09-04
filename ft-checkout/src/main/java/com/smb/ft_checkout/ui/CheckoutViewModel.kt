07e7f964bbd849c741051001009231656ad7a737
smoralber <smoralber@gmail.com>
2022-09-04 15:41:42 -0400

    Call localsource to get data

--- ft-checkout/src/main/java/com/smb/ft_checkout/ui/CheckoutViewModel.kt
+++ ft-checkout/src/main/java/com/smb/ft_checkout/ui/CheckoutViewModel.kt
@@ -1,14 +1,26 @@
 package com.smb.ft_checkout.ui
 
 import androidx.lifecycle.MutableLiveData
+import com.smb.core.extensions.execute
+import com.smb.core.extensions.update
 import com.smb.core.presentation.base.BaseViewModel
 import com.smb.ft_checkout.domain.model.CheckoutModel
+import com.smb.ft_checkout.domain.usecase.GetShoppingCartItemsUseCase
 
-class CheckoutViewModel : BaseViewModel<CheckoutState>() {
+class CheckoutViewModel(
+    private val getShoppingCartItemsUseCase: GetShoppingCartItemsUseCase
+) : BaseViewModel<CheckoutState>() {
 
     val items = MutableLiveData<List<CheckoutModel>>()
 
     internal fun initialize() {
-
+        execute {
+            getShoppingCartItemsUseCase(Unit).fold(
+                handleSuccess = {
+                    items update it
+                },
+                handleError = {}
+            )
+        }
     }
 }
