b01ee610c6588ea518574b96878cdf9a816e858e
smoralber <smoralber@gmail.com>
2022-09-06 16:37:00 -0400

    Refactor method to update quantity for same item

--- core/src/main/java/com/smb/core/data/dataStore/source/LocalSource.kt
+++ core/src/main/java/com/smb/core/data/dataStore/source/LocalSource.kt
@@ -4,12 +4,18 @@ import androidx.datastore.core.DataStore
 import com.smb.core.ShoppingCart
 import com.smb.core.data.dataStore.source.mapper.LocalDataMapper
 import com.smb.core.domain.dataStore.model.ProductModel
+import com.smb.core.extensions.DEFAULT_INT
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 interface LocalSource {
     suspend fun getItems(): Flow<List<ProductModel>>
     suspend fun addNewItem(newItem: ProductModel)
+    suspend fun clearItem(productId: String): Flow<Boolean>
+    suspend fun clearAllItems(): Flow<Boolean>
 }
 
 class LocalSourceImpl(
@@ -25,16 +31,37 @@ class LocalSourceImpl(
     override suspend fun addNewItem(newItem: ProductModel) {
         val item = mapper.toDataModel(newItem)
         dataStore.updateData { shoppingCart ->
-            shoppingCart.toBuilder().apply {
-                if (this.itemsList.contains(item)) {
-                    this.itemsList.find { it.id == newItem.id }.apply {
-                        this?.toBuilder()?.quantity = newItem.quantity
-                    }
-                } else {
-                    this.addItems(item)
-                }
-            }.build()
+            val builder = shoppingCart.toBuilder()
+            val index = builder.itemsList.indexOfFirst { it.id == item.id }
+            if (index != -1) {
+                val storedItem = builder.itemsList[index]
+                val updatedItem =
+                    storedItem.toBuilder().setQuantity(storedItem.quantity.plus(item.quantity))
+                builder.setItems(index, updatedItem)
+            } else builder.addItems(item)
+            builder.build()
         }
     }
+
+    override suspend fun clearItem(productId: String): Flow<Boolean> =
+        flow {
+            dataStore.updateData { shoppingCart ->
+                shoppingCart.toBuilder().apply {
+                    this.itemsList.find {
+                        it.id == productId
+                    }?.let {
+                        emit(this.itemsList.remove(it))
+                    }
+                }.build()
+            }
+        }.flowOn(Dispatchers.IO)
+
+    override suspend fun clearAllItems() =
+        flow {
+            dataStore.updateData { it.toBuilder().clearItems().build() }
+            dataStore.data.map {
+                emit(it.itemsCount == DEFAULT_INT)
+            }
+        }.flowOn(Dispatchers.IO)
 
 }
